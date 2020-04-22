cec847531da7ed9622b6f048293ad59c79a66786
Marina PHILIPPE <marina.philippe@outlook.com>
2020-04-22 15:53:51 +0200

    Ajout DataContext.

---
+++ VisitMyCities.DataModel/DataAccessLayer/VisitMyCitiesContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VisitMyCities.DataModel.BusinessObjects;
+
+namespace VisitMyCities.DataModel.DataAccessLayer
+{
+    public class VisitMyCitiesContext : DbContext
+    {
+        public VisitMyCitiesContext(DbContextOptions<VisitMyCitiesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Batiment> Batiments { get; set; }
+        public DbSet<DetailArchitectural> DetailsArchi { get; set; }
+        public DbSet<Ville> Villes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Batiment>().ToTable("Batiment");
+            modelBuilder.Entity<DetailArchitectural>().ToTable("DetailArchitectural");
+            modelBuilder.Entity<Ville>().ToTable("Ville");
+        }
+    }
+}
