c0ae2b25e3cf56f6099657bdfdbd9091e2c4e229
Marina PHILIPPE <marina.philippe@outlook.com>
2020-05-19 19:10:09 +0200

    Ajout ViewModel avec Batiment et Ville.

---
+++ VisitMyCities/Models/CitiesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisitMyCities.DataModel.BusinessObjects;
+
+namespace VisitMyCities.Models
+{
+    public class CitiesViewModel
+    {
+        public Batiment Batiment { get; set; }
+        public Ville Ville { get; set; }
+    }
+}
