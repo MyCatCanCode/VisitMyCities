0306be0c39f3eb5287328132757fe41e8e56a2aa
Marina PHILIPPE <marina.philippe@outlook.com>
2020-07-05 15:29:06 +0200

    API : ajout détails architecturaux pour un bâtiment donné.

--- VisitMyCities/Controllers/API/BatimentsController.cs
+++ VisitMyCities/Controllers/API/BatimentsController.cs
@@ -11,7 +11,7 @@ using VisitMyCities.Models.DTOs;
 
 namespace VisitMyCities.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[Action]")]
     [ApiController]
     public class BatimentsController : ControllerBase
     {
@@ -27,6 +27,42 @@ namespace VisitMyCities.Controllers.API
         {
             var batiments = _repos.GetAll<Batiment>();
             return batiments.Select(b => new BatimentDTO(b));
+        }
+
+        // GET: api/Villes/DetailsParBatiment/5
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<DetailArchitectural>> DetailsParBatiment(int id)
+        {
+            var batimentRecherche = _repos.GetById<Batiment>(id);
+
+            if (batimentRecherche == null)
+            {
+                return NotFound();
+            }
+
+            var batiments = _repos.GetAll<Batiment>();
+            var details = _repos.GetAll<DetailArchitectural>();
+            var ListeDetailParBatiment = new List<DetailArchitectural>();
+
+            var query = from batiment in batiments
+                        join detail in details on batiment.BatimentId equals detail.BatimentId
+                        where batiment.BatimentId == batimentRecherche.BatimentId
+                        select new DetailArchitectural
+                        {
+                            NomDetail = detail.NomDetail,
+                            DescriptionDetail = detail.DescriptionDetail
+                        };
+
+
+            foreach (var detail in query)
+            {
+                Console.WriteLine(detail);
+                ListeDetailParBatiment.Add(detail);
+
+            }
+
+
+            return ListeDetailParBatiment;
         }
 
         // GET: api/Batiments/5
